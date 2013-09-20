eb3398caa001fac9a99c8ddf1195a24d076e9720
Jason Aller <jraller@ucdavis.edu>
2013-09-20 15:03:54 -0700

    added cascade manual test in support of issue #2

---
+++ test/cascade.js
@@ -0,0 +1,89 @@
+'use strict';
+
+/*
+ * manually run with mocha -R spec -u exports .\test\cascade.js
+ */
+
+var soap = require('..'),
+	client = {},
+	inquirer = require('inquirer'),
+	assert = require('assert'),
+	url = 'http://conference.cascadeserver.com',
+	ws = '/ws/services/AssetOperationService?wsdl',
+	argsOrder = {
+		authentication: {
+			password: '',
+			username: ''
+		}
+	},
+	argsUnorder = {
+		authentication: {
+			username: '',
+			password: ''
+		}
+	},
+	questions = [
+		{
+			type: 'input',
+			name: 'serverUrl',
+			message: 'Server url: ',
+			'default': 'http://conference.cascadeserver.com'
+		},
+		{
+			type: 'input',
+			name: 'username',
+			message: 'Username: ',
+			'default': 'jraller'
+		},
+		{
+			type: 'password',
+			name: 'password',
+			message: 'Password: '
+		}
+	];
+
+module.exports = {
+	'input': {
+		'createClient': function (done) {
+			this.timeout(60 * 1000);
+			inquirer.prompt(questions, function (answers) {
+				url = answers.serverUrl + ws;
+				argsOrder.authentication.username = answers.username;
+				argsOrder.authentication.password = answers.password;
+				argsUnorder.authentication.username = answers.username;
+				argsUnorder.authentication.password = answers.password;
+				done();
+			});
+		}
+	},
+	'args order': {
+		'createClient': function (done) {
+			this.timeout(5000);
+			soap.createClient(url, function (err, newClient) {
+				assert.ok(!err);
+				client = newClient;
+				done();
+			});
+		},
+		'listSites with correct order': function (done) {
+			this.timeout(5000);
+			client.listSites(argsOrder, function (err, response) {
+				assert.ok(!err);
+				if (!err) {
+					assert.equal(response.listSitesReturn.success.toString(), 'true');
+				}
+				done();
+			});
+		},
+		'listSites with incorrect order': function (done) {
+			this.timeout(5000);
+			client.listSites(argsUnorder, function (err, response) {
+				assert.ok(!err);
+				if (!err) {
+					assert.equal(response.listSitesReturn.success.toString(), 'true');
+				}
+				done();
+			});
+		}
+	}
+};
